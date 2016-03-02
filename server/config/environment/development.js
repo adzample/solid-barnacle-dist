89daab504032a29a3a0e6ba9e8c17148786def34
adzample <adzample@gmail.com>
2016-03-02 13:06:16 +1100

    new additions

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/solidbarnacle-dev'
+  },
+
+  seedDB: true
+};
