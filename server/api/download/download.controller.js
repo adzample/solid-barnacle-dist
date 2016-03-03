6d44f141c2f0c4a57e9064dd8fd0a38282f8d2fc
adzample <adzample@gmail.com>
2016-03-03 19:54:36 +1100

    changes

--- server/api/download/download.controller.js
+++ server/api/download/download.controller.js
@@ -7,7 +7,7 @@ exports.index = function(req, res) {
   var query = req.body.dl
   // 'mbvMh7l5NtIB8f4.pdf'
   if(query === 'mbvMh7l5NtIB8f4'){
-    res.download('./server/api/download/mbvMh7l5NtIB8f4.pdf', 'acethecfa-25-practice-questions.pdf')
+    res.download('./server/api/download/mbvMh7l5NtIB8f4.pdf')
   }else{
     res.sendStatus(302)
   }
